c976d113aa7b22234c7deaad0ec17268e233b2e8
PonomareVlad <Git@PonomareVlad.ru>
2020-11-24 23:47:18 +0500

    Import mjs

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+import test from "./import"
+
 export default async (req, res) => {
     res.json({
         body: req.body,
         query: req.query,
         cookies: req.cookies,
+        import: test
     })
 }
