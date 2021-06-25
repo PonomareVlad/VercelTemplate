61e519c25e5f2b2e140d2c06025cbb5c9f269c2b
PonomareVlad <Git@PonomareVlad.ru>
2021-06-25 05:06:15 +0500

    Zero-config and .mjs extensions

---
+++ api/index.mjs
@@ -0,0 +1,10 @@
+import test from '../import.mjs';
+
+export default async (req, res) => {
+    res.json({
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+        test
+    })
+}
