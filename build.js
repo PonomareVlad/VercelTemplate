3f4aef3e1c42f31aaa12e8726a4e2fcb1b0cc0c7
PonomareVlad <Git@PonomareVlad.ru>
2021-09-06 13:56:06 +0500

    Test custom builder

--- build.js
+++ build.js
@@ -13,11 +13,11 @@ const images = {
         1920
     ]
 }
-const path = path.resolve(__dirname, './.vercel_build_output/config');
-fs.mkdir(path, {recursive: true}, (err) => {
+const dir = path.resolve(__dirname, './.vercel_build_output/config');
+fs.mkdir(dir, {recursive: true}, (err) => {
     if (err) throw err;
 });
-fs.writeFile(path + '/images.json', JSON.stringify(images), (err) => {
+fs.writeFile(dir + '/images.json', JSON.stringify(images), (err) => {
     if (err) throw err;
     console.log('Images configuration file created successfully!');
 });
