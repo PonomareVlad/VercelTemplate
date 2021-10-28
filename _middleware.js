ee34c146049f391d2fe8187d24a6cc1f3322c4ed
PonomareVlad <Git@PonomareVlad.ru>
2021-10-28 10:36:55 +0500

    Middleware test

--- _middleware.js
+++ _middleware.js
@@ -1,13 +1,3 @@
 export default function middleware(req, ev) {
-    console.dir(req)
-    const {url, geo} = req;
-    // const data = {ip: req.ip, geo: req.geo, ua: req.ua};
-    if (geo) {
-        const country = geo.country || 'US'
-        const city = geo.city || 'San Francisco'
-        const region = geo.region || 'CA'
-        url.searchParams.set('country', country)
-        url.searchParams.set('city', city)
-        url.searchParams.set('region', region)
-    }
+    console.log(JSON.stringify(req))
 }
