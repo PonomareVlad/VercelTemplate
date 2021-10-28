94be7f93d7913674cb7615421a8247fa55bac9fd
PonomareVlad <Git@PonomareVlad.ru>
2021-10-28 10:05:19 +0500

    Middleware test

--- _middleware.js
+++ _middleware.js
@@ -1,8 +1,5 @@
 export default function middleware(req, ev) {
-    console.log('Edit and run at the edge!')
-    return new Response({
-        ip: req.ip,
-        geo: req.geo,
-        ua: req.ua
-    })
+    const data = {ip: req.ip, geo: req.geo, ua: req.ua}
+    console.dir(data)
+    return new Response(JSON.stringify(data))
 }
