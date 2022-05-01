1f34351e9a50220107867d8e2e858f1a5ce7a77e
PonomareVlad <Git@PonomareVlad.ru>
2022-05-01 16:32:21 +0500

    Sharp package test

--- index.mjs
+++ index.mjs
@@ -1,9 +1,12 @@
+import sharp from "sharp";
+
 export default async (req, res) => {
-    res.json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
-        headers: req.headers,
-        versions: process.versions
-    })
+    return res.end(await sharp({
+        create: {
+            width: 48,
+            height: 48,
+            channels: 4,
+            background: {r: 255, g: 0, b: 0, alpha: 0.5}
+        }
+    }).png().toBuffer())
 }
