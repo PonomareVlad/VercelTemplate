2fa749e189242545e7f5627a5f07108f3efb00bd
PonomareVlad <Git@PonomareVlad.ru>
2022-08-08 19:58:04 +0500

    Excluded api

--- build.mjs
+++ build.mjs
@@ -4,7 +4,7 @@ const rootDir = new URL('./', import.meta.url),
     outputDir = new URL('./.vercel/output/', rootDir),
     configPath = new URL('./config.json', outputDir),
     staticDir = new URL('./static/', outputDir),
-    excluded = ['.', 'node_modules'],
+    excluded = ['.', 'node_modules', 'api'],
     config = {
         version: 3,
         images: {sizes: [128, 1024, 2048], formats: ["image/webp", "image/avif"], domains: ["cloudflare-ipfs.com"]}
