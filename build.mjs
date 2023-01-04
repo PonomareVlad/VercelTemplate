35a2515b04dc36ed1d8944c3c90f5ae1888d2249
Vladislav Ponomarev <Git@PonomareVlad.ru>
2023-01-04 14:46:48 +0500

    GitHub Actions build test

---
+++ build.mjs
@@ -0,0 +1,12 @@
+import {dirname} from "path";
+import {writeFileSync, mkdirSync} from 'fs';
+
+const configPath = `.vercel/output/functions/api/index.prerender-config.json`;
+
+const config = {
+    "expiration": false,
+    "bypassToken": process.env.BYPASS_TOKEN
+}
+
+mkdirSync(dirname(configPath), {recursive: true});
+writeFileSync(configPath, JSON.stringify(config), {});
